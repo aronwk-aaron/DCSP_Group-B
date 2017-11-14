87b3359d6436ae9e79eac88a054c9efc1a1eaec1
Drumanaut <33577195+Drumanaut@users.noreply.github.com>
2017-11-14 15:04:56 -0600

    login for db access

---
+++ src/login.php
@@ -0,0 +1,6 @@
+<?php
+  $hn = 'localhost';  // hostname
+  $db = 'awk73';  // database name 
+  $un = 'awk73';  // username (root on XAMPP, netID on pluto)
+  $pw = 'stars';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
+?>
