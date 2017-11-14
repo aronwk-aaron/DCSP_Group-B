468a9e313f257c81b6c24511385048bc7b791e86
Drumanaut <33577195+Drumanaut@users.noreply.github.com>
2017-11-14 15:16:17 -0600

    Revert "login for db access"
    This reverts commit 87b3359d6436ae9e79eac88a054c9efc1a1eaec1.

--- src/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $hn = 'localhost';  // hostname
-  $db = 'awk73';  // database name 
-  $un = 'awk73';  // username (root on XAMPP, netID on pluto)
-  $pw = 'stars';  // MySQL password (empty on XAMPP, your MySQL password on pluto)
-?>
