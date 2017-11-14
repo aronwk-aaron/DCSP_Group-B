ab219ef9ec03d0427c5f7708d8b2fd5db178e04d
Drumanaut <33577195+Drumanaut@users.noreply.github.com>
2017-11-14 15:02:59 -0600

    Search and Browwse
    I still need to get them into a format that actually looks good, but the basic functionality is there

--- src/browse.php
+++ src/browse.php
@@ -3,7 +3,53 @@
 ?>
 
 <div class="container">
-	This is the Browse Page
+	<?php
+browse();
+  
+function browse()
+  {
+    //performs query for all inventory
+      
+    //database query section
+    require_once 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) 
+      die($conn->connect_error);
+
+    $query  = "SELECT isbn, title, author, publisher, genre, price, quantity, inStock FROM nb_Inventory";
+
+    $result = $conn->query($query);
+    if (!$result) 
+      die($conn->error);
+
+    $rows = $result->num_rows;
+      
+    echo'<table>';
+    echo'<tr><th>Author</th><th>Title</th><th>Genre</th><th>ISBN</th><th>Publisher</th><th>Price</th><th>Quantity</th><th>In Stock</th></tr>';
+      
+    //table display
+    for ($j = 0 ; $j < $rows ; ++$j)
+    {
+      $result->data_seek($j);
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      $inStock = "No";
+      if($row['inStock'] == 1)
+        $inStock = "Yes";
+      
+
+      echo '<tr><td>' . $row['author']    . '</td>';
+      echo '<td>'     . $row['title']     . '</td>';
+      echo '<td>'     . $row['genre']     . '</td>';
+      echo '<td>'     . $row['isbn']      . '</td>';
+      echo '<td>'     . $row['publisher'] . '</td>';
+      echo '<td>'     . $row['price']     . '</td>';
+      echo '<td>'     . $row['quantity']  . '</td>';
+      echo '<td>'     . $inStock          . '</td></tr>';
+    }
+    
+    echo'</table>';
+  }
+?>
 </div>
 
 <?php
--- src/search.php
+++ src/search.php
@@ -3,7 +3,92 @@
 ?>
 
 <div class="container">
-	This is the search Page
+	<?php
+  
+  $term = $category = "";
+  
+  if($_POST)
+  {
+    $term = $_POST["term"];
+    $category = $_POST["category"];
+    $searchTerm = $term;
+  }
+?>
+
+
+<html lang="en">
+    <head>
+        <title>Search Test</title>
+    </head>
+    <body>
+        <h1>Search Test</h1>
+        
+        <p>Select a category and enter your search.</p>
+        <form method = "POST" action="SearchTest.php">
+        <select name = "category">
+        <form>
+        <option <?php if($category == "Author"){echo("selected");}?>>Author</option>
+        <option <?php if($category == "Title"){echo("selected");}?>>Title</option>
+        <option <?php if($category == "Genre"){echo("selected");}?>>Genre</option>
+        <input type="text" name="term" value= <?php echo ($term);?>>
+        <input type="submit">
+        </form>  
+        
+    </body>
+</html>
+
+
+<?php 
+  if($_POST)
+    search($category, $searchTerm);
+?>
+
+<?php  
+  function search($field, $term)
+  {
+    //database query section
+    require_once 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) 
+      die($conn->connect_error);
+
+    $query  = "SELECT isbn, title, author, publisher, genre, price, quantity, inStock FROM nb_Inventory WHERE ";
+    $query .= $field . " = '" . $term . "'"; 
+    if(!$term)
+      $query = "SELECT isbn, title, author, publisher, genre, price, quantity, inStock FROM nb_Inventory";
+    
+    $result = $conn->query($query);
+    if (!$result) 
+      die($conn->error);
+
+    $rows = $result->num_rows;
+    
+    echo'<table>';
+    echo'<tr><th>Author</th><th>Title</th><th>Genre</th><th>ISBN</th><th>Publisher</th><th>Price</th><th>Quantity</th><th>In Stock</th></tr>';
+      
+    //table display
+    for ($j = 0 ; $j < $rows ; ++$j)
+    {
+      $result->data_seek($j);
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      $inStock = "No";
+      if($row['inStock'] == 1)
+        $inStock = "Yes";
+      
+
+      echo '<tr><td>' . $row['author']    . '</td>';
+      echo '<td>'     . $row['title']     . '</td>';
+      echo '<td>'     . $row['genre']     . '</td>';
+      echo '<td>'     . $row['isbn']      . '</td>';
+      echo '<td>'     . $row['publisher'] . '</td>';
+      echo '<td>'     . $row['price']     . '</td>';
+      echo '<td>'     . $row['quantity']  . '</td>';
+      echo '<td>'     . $inStock          . '</td></tr>';
+    }
+    
+    echo'</table>';      
+  }  
+?>  
 </div>
 
 <?php
