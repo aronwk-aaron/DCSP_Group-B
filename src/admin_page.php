38fe4e3c1624c4b926af18b6b56159fa7b3b7de3
dcw293 <31903898+dcw293@users.noreply.github.com>
2017-11-26 17:51:21 -0600

    Admin_page
    Admin_page now has view purchase history, stock increase, and delete_users capabilities.

--- src/admin_page.php
+++ src/admin_page.php
@@ -1,8 +1,21 @@
 <?php
 	require_once("templates/header.php");
 	//kicking out the wrong people
+
 	if(isset($_SESSION['username'])){
 		if($is_admin){
+            require_once 'inc/login.php';
+            $conn = new mysqli($hn, $un, $pw, $db);
+            if ($conn->connect_error)
+                die($conn->connect_error);
+
+            //history query
+            $hquery  = "SELECT I.isbn, I.price, H.orderNum, uH.datePurch, uH.dueDate, uT.username  FROM nb_userHistory uH, nb_History H, nb_Inventory I, nb_userstable uT WHERE H.userID = uT.userID AND uH.orderNum = H.orderNum AND uH.bookID = I.bookID";
+            //inventory query
+            $hresult = $conn->query($hquery);
+            if (!$hresult)
+                die($conn->error);
+            $hrows = $hresult->num_rows;
 ?>
 			<script type="text/javascript">
 				var page_title = "NetBooks - Admin";
@@ -19,9 +32,8 @@
 				      	<?php print($_SESSION['firstname']);?>
 				      	<?php print($_SESSION['lastname']);?>
 				      </h4>
-				      <p class="card-text">
-				      	You are the boss
-				      	<!-- put other useful info here -->
+				      <br class="card-text">
+                        You are a librarian of Netbooks. Review all purchase histories, users, and add stock to inventory.
 				      <p>
 				    </div>
 				  </div>
@@ -34,7 +46,7 @@
 						    <a class="nav-link active" id="history-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">History</a>
 						  </li>
 						  <li class="nav-item">
-						    <a class="nav-link" id="inventory-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Inventory</a>
+						    <a class="nav-link" id="stock-tab" data-toggle="tab" href="#stock" role="tab" aria-controls="stock" aria-selected="false">In Stock</a>
 						  </li>
 						  <li class="nav-item">
 						    <a class="nav-link" id="users-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Users</a>
@@ -42,13 +54,190 @@
 						</ul>
 						<div class="tab-content" id="myTabContent">
 						  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="history-tab">
-						  	Just do it
+                              <div class="container">
+                                  </br>
+                                  <div class="card">
+                                      <div class="card-body">
+                                          <table id="table_id" class="display compact" >
+                                              <thead>
+                                              <tr>
+                                                  <th>  Order Number             </th>
+                                                  <th>  ISBN                     </th>
+                                                  <th>  Price                    </th>
+                                                  <th>  Date Purchased           </th>
+                                                  <th>  Date Due                 </th>
+                                                  <th>  User ID                  </th>
+                                              </tr>
+                                              </thead>
+                                              <tbody>
+                                              <?php
+
+                                              //table display
+                                              for ($j = 0 ; $j < $hrows ; ++$j)
+                                              {
+                                              $hresult->data_seek($j);
+                                              $hrow = $hresult->fetch_array(MYSQLI_ASSOC);
+                                              ?>
+                                              <tr>
+                                                  <td>      <?php print($hrow['orderNum']);   ?>     </td>
+                                                  <td>      <?php print($hrow['isbn']);       ?>     </td>
+                                                  <td>     $<?php print($hrow['price']);      ?>.00  </td>
+                                                  <td>      <?php print($hrow['datePurch']);  ?>     </td>
+
+                                                  <?php
+                                                  if($hrow['dueDate'] == NULL) {
+                                                      ?>
+                                                      <td> No Due Date</td>
+                                                      <?php
+                                                  }
+                                                  else{
+                                                      ?>
+                                                      <td>     <?php print($hrow['dueDate']); ?>     </td>
+                                                      <?php
+                                                  }
+                                                  ?>
+                                                  <td>
+                                                      <?php print($hrow['username']);     ?>
+                                                  </td>
+                                                  <?php
+                                              }
+                                                  ?></tr>
+                                              </tbody>
+                                          </table>
+                                      </div>
+                                  </div>
+                              </div>
 						  </div>
-						  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="inventory-tab">
-						  	Don't let your memes be dreams
-						  </div>
+						  <div class="tab-pane fade" id="stock" role="tabpanel" aria-labelledby="Add Stock-tab">
+                              <div class="container">
+                                  </br>
+                                  <div class="card">
+                                      <div class="card-body">
+                                          <h3 class="card-title">Add Stock</h3>
+                                          <?php
+                                          if($_POST)
+                                         {
+                                             if($_POST['confirm']) {
+
+                                                 $bookid = $_POST['confirm'];
+                                                 if ($_POST['quantity'] > 0) {
+
+                                                     $qquery = "SELECT quantity FROM nb_inventory WHERE bookID = '$bookid'";
+                                                     $qresult = $conn->query($qquery);
+                                                     if (!$qresult)
+                                                         die($conn->error);
+                                                     $qresult->data_seek(0);
+                                                     $qresults = $qresult->fetch_array(MYSQLI_ASSOC);
+                                                     $finalQuant = $qresults['quantity'] + $_POST['quantity'];
+                                                     $qquery = "UPDATE nb_inventory SET quantity = '$finalQuant' WHERE bookID = '$bookid'";
+                                                     if (!$conn->query($qquery)) {
+                                                         echo "Error updating record.";
+                                                     }
+                                                 } else {
+                                                     print("Please enter a number greater than zero");
+                                                 }
+                                             }
+                                         }
+
+                                          $iquery  = "SELECT * FROM nb_Inventory";
+
+                                          $iresult = $conn->query($iquery);
+                                          if (!$iresult)
+                                              die($conn->error);
+
+                                          $irows = $iresult->num_rows;
+                                          ?>
+                                          <table id="table_id" class="display compact" >
+                                              <thead>
+                                              <tr>
+                                                  <th>  Title     </th>
+                                                  <th>  ISBN      </th>
+                                                  <th>  Price     </th>
+                                                  <th>  Quantity  </th>
+                                                  <th>  Quantity Change</th>
+                                              </tr>
+                                              </thead>
+                                              <tbody>
+                                              <?php
+
+                                              //table display
+                                              for ($j = 0 ; $j < $irows ; ++$j)
+                                              {
+                                                  $iresult->data_seek($j);
+                                                  $irow = $iresult->fetch_array(MYSQLI_ASSOC);
+                                                  ?>
+                                                  <tr>
+                                                  <td>      <?php print($irow['title']);      ?>      </td>
+                                                  <td>      <?php print($irow['isbn']);       ?>      </td>
+                                                  <td>     $<?php print($irow['price']);      ?>.00   </td>
+                                                  <td>      <?php print($irow['quantity']);   ?>      </td>
+                                                  <td>
+                                                      <form action="admin_page.php#stock" method="post">
+                                                          <input name="quantity" class="form-control input-number" value="1" min="1" max="10" type="text">
+                                                          <button type="submit" class="btn btn-success" name="confirm" value="<?php echo($irow['bookID']);?>">Submit</button>
+                                                      </form>
+                                                  </td>
+                                                      <?php
+                                              }
+
+                                              ?></tbody>
+                                          </table>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
 						  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="users-tab">
-						  	Just Freaking do it
+                              <div class="container">
+                                  </br>
+                                  <div class="card">
+                                      <div class="card-body">
+                                          <table id="table_id" class="display compact" >
+                                              <thead>
+                                              <tr>
+                                                  <th>  User Name                </th>
+                                                  <th>  User ID                  </th>
+                                                  <th>  Delete User?             </th>
+                                              </tr>
+                                              </thead>
+                                              <tbody>
+                                              <?php
+                                              if($_POST)
+                                              {
+                                                  if($_POST['delete']) {
+                                                      $delete_user = $_POST['delete'];
+                                                      $dquery = "DELETE FROM nb_userstable WHERE userID = '$delete_user'";
+                                                      if (!$conn->query($dquery)) {
+                                                          echo "Error deleting record.";
+                                                      }
+                                                  }
+                                              }
+                                              $uquery  = "SELECT * FROM nb_userstable";
+
+                                              $uresult = $conn->query($uquery);
+                                              if (!$uresult)
+                                                  die($conn->error);
+
+                                              $urows = $uresult->num_rows;
+                                              //table display
+                                              for ($j = 0 ; $j < $urows ; ++$j)
+                                              {
+                                              $uresult->data_seek($j);
+                                              $urow = $uresult->fetch_array(MYSQLI_ASSOC);
+                                              ?>
+                                              <tr>
+                                                  <td>      <?php print($urow['userName']);   ?>     </td>
+                                                  <td>      <?php print($urow['userID']);     ?>     </td>
+                                                  <td>      <form action="admin_page.php#stock" method="post">
+                                                                <button type="submit" class="btn btn-success" name="delete" value="<?php echo($urow['userID']);?>">Delete</button>
+                                                            </form>
+                                                  </td>
+                                              </tr>
+                                              <?php } ?>
+                                              </tbody>
+                                          </table>
+                                      </div>
+                                  </div>
+                              </div>
 						  </div>
 						</div>
 				      </p>
@@ -59,11 +248,11 @@
 			</div>
 
 <?php
-		}
-		else{
-		  header("Location: user_page.php" );
-		  exit();
-		}
+                }
+                else{
+                header("Location: user_page.php" );
+                exit();
+                }
 	}
 	else {
 		header("Location: index.php" );
