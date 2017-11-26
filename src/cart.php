31b390c1aae390e70974642c64429cbd7db5599d
Unknown <chris.allen070@gmail.com>
2017-11-26 10:53:28 -0600

    Cart Page WIP
    WIP Cart Page

--- src/cart.php
+++ src/cart.php
@@ -14,7 +14,63 @@
 ?>
 
 	<div class="container">
-		This is the cart Page
+
+  <?php
+    require_once 'inc/login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) 
+      die($conn->connect_error);
+      
+      
+    // Perform two queries to get the user's ID, then use that to get info from the cart     
+    $query = "SELECT userID FROM nb_UsersTable WHERE userName = $_SESSION['userName']";
+	  $result = $connection->query($query);
+
+	  if (!$result) die($conn->error);
+	  else if ($result->num_rows)
+	  {
+	      $row = $result->fetch_array(MYSQLI_ASSOC);
+	      $result->close();
+           
+        $userID = $row['userID'];
+           
+    $query  = "SELECT I.title, I.ISBN, I.price, uC.isRent FROM nb_Carts C, nb_userCarts uC, nb_Inventory I WHERE C.userID = $userID AND uC.cartID = C.cartID AND uC.bookID = I.bookID ;";
+
+    $result = $conn->query($query);
+    if (!$result) 
+      die($conn->error);
+
+    $rows = $result->num_rows;
+    
+    if $rows == 0 {
+      echo 'There is nothing in the cart!'
+    }  
+    
+    else{
+      echo'<table>';
+      echo'<tr><th>Title</th><th>ISBN</th><th>Price</th><th>Rent?</th></tr>';
+        
+      //table display
+      for ($j = 0 ; $j < $rows ; ++$j)
+      {
+        $result->data_seek($j);
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        $isRent = "No";
+        if($row['uC.isRent'] == 1)
+          $isRent = "Yes";
+        
+  
+        echo '<tr><td>' . $row['I.title']    . '</td>';
+        echo '<td>'     . $row['I.ISBN']     . '</td>';
+        echo '<td>'     . $row['I.price']    . '</td>';
+        echo '<td>'     . $inStock           . '</td></tr>';
+      }
+      
+      echo'</table>';
+    }
+    
+    $connection->close();           
+  ?>
 	</div>
 
 <?php
